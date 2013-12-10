08e87746209e33bb1a9f45bfdb06ffbc12e8d88e
António P. P. Almeida <appa@perusio.net>
2013-12-10 20:43:36 +0100

    * Added some documentation.

--- vars.sh
+++ vars.sh
@@ -2,5 +2,8 @@
 
 ## The base directory for the Nginx source code.
 
+############ CONFIGURE THIS ############.
+## Set this to be the directory where you have untarred/unzipped the
+## nginx source archive you downloaded.
 NGINX_BASE_DIR=$HOME/C/nginx
 
